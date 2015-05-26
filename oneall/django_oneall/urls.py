48c4828353eb1f4f119fa49e0f5e750f2040eb33
Ekevoo <ekevoo@gmail.com>
2015-05-26 10:33:00 -0300

    Easier urls.py maintainability.

--- oneall/django_oneall/urls.py
+++ oneall/django_oneall/urls.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-from .views import oa_login, oa_logout, oa_profile, oa_nosocial
-
+from . import views
 
 urlpatterns = [
-    url('^login', oa_login, name='oneall-login'),
-    url('^logout', oa_logout, name='oneall-logout'),
-    url('^profile', oa_profile, name='oneall-profile'),
-    url('^nosocial', oa_nosocial, name='oneall-nosocial')
+    url('^login', views.oa_login, name='oneall-login'),
+    url('^logout', views.oa_logout, name='oneall-logout'),
+    url('^profile', views.oa_profile, name='oneall-profile'),
+    url('^nosocial', views.oa_nosocial, name='oneall-nosocial')
 ]
