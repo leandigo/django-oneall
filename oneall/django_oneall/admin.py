af8ac0c058f01f7033399413ecce6b96ea6f2777
Ekevoo <ekevoo@gmail.com>
2015-05-28 18:18:22 -0300

    PEP-8 goodness.

--- oneall/django_oneall/admin.py
+++ oneall/django_oneall/admin.py
@@ -1,10 +1,10 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 from .models import *
+
 
 @admin.register(OneAllUserIdentity, app_label='oneall')
 class IdentityAdmin(admin.ModelAdmin):
     readonly_fields = ('raw',)
     list_display = ('user_token', 'user', 'raw')
-
