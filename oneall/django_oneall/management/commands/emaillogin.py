6cfc9de7fe8fd048a75845a69bdeefc7c742bae4
Ekevoo <ekevoo@gmail.com>
2015-07-10 18:14:19 -0300

    Add the possibility of testing SMTP from the command-line.

--- oneall/django_oneall/management/commands/emaillogin.py
+++ oneall/django_oneall/management/commands/emaillogin.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand
 from django.core.urlresolvers import reverse
 
@@ -6,14 +7,27 @@ from ...auth import EmailTokenAuthBackend
 
 
 class Command(BaseCommand):
-    help = "E-mail login without sending the actual e-mail."
+    help = "Issues an e-mail login token."
 
     def add_arguments(self, parser):
+        parser.add_argument('-s', '--send', dest='send', action='store_true',
+                            help="Actually e-mail the token instead of only displaying it.")
         parser.add_argument('email', type=str)
 
-    def handle(self, email, **options):
+    def handle(self, email, send, **options):
         if '@' not in email:
             self.stderr.write("Failed. E-mail is mandatory.")
-            return 1
+            return
         query_string = EmailTokenAuthBackend().issue(email)
-        self.stdout.write("Complete login with: %s?%s" % (reverse('oneall-login'), query_string))
+        msg = "Complete login with: %s?%s" % (reverse('oneall-login'), query_string)
+        self.stdout.write(msg)
+        if send:
+            mail = EmailMessage()
+            mail.to = [email]
+            mail.subject = 'Login Test'
+            mail.body = msg
+            try:
+                sent = mail.send()
+                self.stdout.write("Sent %d message." % sent)
+            except ConnectionError as e:
+                self.stderr.write(str(e))
