3d332084554e255785ee1331c495a792e4b33d9b
Ekevoo <ekevoo@gmail.com>
2015-10-04 22:59:18 -0400

    Added Internationalization.

--- django_oneall/forms.py
+++ django_oneall/forms.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.utils.translation import ugettext_lazy
 
 
 class EmailForm(forms.Form):
-    email = forms.EmailField()
+    email = forms.EmailField(label=ugettext_lazy("E-mail"))
