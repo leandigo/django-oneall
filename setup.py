8d9e8239d36aa78acbda8ebc0340d0bf0b3dac70
Ekevoo <ekevoo@gmail.com>
2016-02-22 22:23:28 -0500

    1.1.2 version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 README = open(join(dirname(__file__), 'README.rst')).read()
 setup(
     name='django-oneall',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     package_data={
         'django_oneall/templates/oneall': [
